948bb8f786ac895c950d7b6700eb8713eb2c6f76
Jang Sungil <jasuil@daum.net>
2023-06-22 14:47:44 +0900

    불필요한 repository annotation 삭제

--- src/main/java/com/fooddiary/api/repository/UserRepository.java
+++ src/main/java/com/fooddiary/api/repository/UserRepository.java
@@ -1,15 +1,14 @@
 package com.fooddiary.api.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.fooddiary.api.entity.user.Status;
 import com.fooddiary.api.entity.user.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
-@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
     List<User> findByEmail(String email);
+
     User findByEmailAndStatus(String email, Status status);
 }
