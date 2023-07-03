92cf721a14f6c90ad753cdc53ea57527e3b48408
naver0504 <qortndwls1234@gmail.com>
2023-07-04 06:27:58 +0900

    정상적으로 파일 받을 수 있게 수정

--- src/main/java/com/fooddiary/api/controller/ImageController.java
+++ src/main/java/com/fooddiary/api/controller/ImageController.java
@@ -5,27 +5,33 @@ import com.fooddiary.api.dto.response.DayImagesDto;
 import com.fooddiary.api.service.DayImageService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
 import java.io.IOException;
+
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @Slf4j
+@RequestMapping("/image")
 public class ImageController {
 
     private final DayImageService dayImageService;
 
-    @PostMapping("/saveImage")
-    public void saveImage(@RequestBody ImageCreateDto imageCreateDto) throws IOException {
+    @PostMapping(value = "/saveImage")
+    public void saveImage(@RequestPart("files") List<MultipartFile> multipartFiles,
+                          @RequestParam("localDateTime") LocalDateTime localDateTime) throws IOException {
 
-        dayImageService.saveImage(imageCreateDto.getMultipartFile(), imageCreateDto.getLocalDateTime());
+       dayImageService.saveImage(multipartFiles, localDateTime);
     }
 
-
     /***
-     * /image?year=2023,month=6,day=30
+     * /image?year=2023&month=6&day=30
      * 하루 사진 받기
      */
     @GetMapping("/image")
@@ -36,14 +42,18 @@ public class ImageController {
 
     /***
      *
-     * /image?year=2023,month=7
+     * /image?year=2023&month=7
      * 한 달의 사진 받기
      */
     @GetMapping("/images")
     public ResponseEntity<List<DayImagesDto>> showImages(@RequestParam int year, @RequestParam int month) {
         List<DayImagesDto> dayImages = dayImageService.getDayImages(year, month);
-
         return ResponseEntity.ok(dayImages);
+    }
+
+    @GetMapping("/test")
+    public String test() {
+        return "image";
     }
 
 
