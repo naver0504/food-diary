eb5cead66df8cb4a791ea3ebd28a5b25efc7fa7e
naver0504 <qortndwls1234@gmail.com>
2023-09-07 01:49:11 +0900

    SaveImageRequestDTO 작성

--- src/main/java/com/fooddiary/api/dto/request/SaveImageRequestDTO.java
+++ src/main/java/com/fooddiary/api/dto/request/SaveImageRequestDTO.java
@@ -1,6 +1,8 @@
 package com.fooddiary.api.dto.request;
 
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.annotation.Nullable;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -11,7 +13,11 @@ import java.time.LocalDateTime;
 @Getter
 public class SaveImageRequestDTO {
 
+    @JsonFormat(shape= JsonFormat.Shape.STRING, pattern="yyyy-MM-dd'T'HH:mm:ss", timezone = "Asia/Seoul")
     private LocalDateTime localDateTime;
+
+    @Nullable
     private Double longitude;
+    @Nullable
     private Double latitude;
 }
