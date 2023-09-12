df50eb33b483b0e419912bfc2557f27d41854519
jasuil <jasuil@daum.net>
2023-09-13 00:30:44 +0900

    공지 목록조회 작성중

--- src/main/java/com/fooddiary/api/repository/NoticeDynamicConditionRepositoryImpl.java
+++ src/main/java/com/fooddiary/api/repository/NoticeDynamicConditionRepositoryImpl.java
@@ -1,9 +1,14 @@
 package com.fooddiary.api.repository;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
@@ -21,15 +26,77 @@ public class NoticeDynamicConditionRepositoryImpl implements NoticeDynamicCondit
 
     @Override
     public long selectCount(String title, String content, Boolean available, LocalDate noticeAt) {
+        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Long> query = cb.createQuery(Long.class);
+        Root<Notice> notice = query.from(Notice.class);
+        List<Predicate> predicateOrList = orCondition(cb, notice, title, content);
+        List<Predicate> predicateAndList = andCondition(cb, notice, available, noticeAt);
+        List<Predicate> predicateList = new ArrayList<>();
+        if (predicateOrList.size() > 0) {
+            predicateList.add(cb.or(predicateOrList.toArray(new Predicate[predicateOrList.size()])));
+        }
+        if (predicateAndList.size() > 0) {
+            predicateList.add(cb.and(predicateAndList.toArray(new Predicate[predicateAndList.size()])));
+        }
+        Predicate[] condition = new Predicate[predicateList.size()];
+        for(int i=0;i < condition.length; i++) condition[i] = predicateList.get(i);
+        query.select(cb.count(notice))
+                .where(condition);
+        return entityManager.createQuery(query).getSingleResult();
+
+/*
         final TypedQuery<Long> jpaQuery = entityManager.createQuery(
                 jpqlPrefix(true, title, content, available, noticeAt), Long.class);
         setParam(jpaQuery, title, content, available, noticeAt);
         return jpaQuery.getSingleResult();
+
+ */
+    }
+
+    private <T> List<Predicate> orCondition (CriteriaBuilder cb, Root<T> notice, String title, String content) {
+        List<Predicate> predicateList = new ArrayList<>();
+        if (title != null) {
+            predicateList.add(cb.like(notice.get("title"), "%" + title + "%"));
+        }
+        if (content != null) {
+            predicateList.add(cb.like(notice.get("content"), "%" + content + "%"));
+        }
+        return predicateList;
+    }
+
+    private <T> List<Predicate> andCondition(CriteriaBuilder cb, Root<T> notice, Boolean available, LocalDate noticeAt) {
+        List<Predicate> predicateList = new ArrayList<>();
+        if (available != null) {
+            predicateList.add(cb.equal(notice.get("available"), available));
+        }
+        if (noticeAt != null) {
+            predicateList.add(cb.equal(notice.get("noticeAt"), noticeAt));
+        }
+        return predicateList;
     }
 
     @Override
     public List<Notice> selectList(String title, String content, Boolean available, LocalDate noticeAt,
                                    Pageable pageable) {
+        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Notice> query = cb.createQuery(Notice.class);
+        Root<Notice> notice = query.from(Notice.class);
+        List<Predicate> predicateOrList = orCondition(cb, notice, title, content);
+        List<Predicate> predicateAndList = andCondition(cb, notice, available, noticeAt);
+        List<Predicate> predicateList = new ArrayList<>();
+        if (predicateOrList.size() > 0) {
+            predicateList.add(cb.or(predicateOrList.toArray(new Predicate[predicateOrList.size()])));
+        }
+        if (predicateAndList.size() > 0) {
+            predicateList.add(cb.and(predicateAndList.toArray(new Predicate[predicateAndList.size()])));
+        }
+        Predicate[] condition = new Predicate[predicateList.size()];
+        for(int i=0;i < condition.length; i++) condition[i] = predicateList.get(i);
+        query.select(notice)
+                .where(condition);
+
+        return entityManager.createQuery(query).getResultList();
+        /*
         final TypedQuery<Notice> jpaQuery = entityManager.createQuery(
                 jpqlPrefix(false, title, content, available, noticeAt), Notice.class);
         setParam(jpaQuery, title, content, available, noticeAt);
@@ -37,7 +104,7 @@ public class NoticeDynamicConditionRepositoryImpl implements NoticeDynamicCondit
         return jpaQuery.setFirstResult((int) pageable.getOffset())
                        .setMaxResults(pageable.getPageSize() + 1)
                        .getResultList();
-
+*/
     }
 
     private static String jpqlPrefix(boolean isCount, String title, String content, Boolean available,
