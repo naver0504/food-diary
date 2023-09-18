8f7594e4bbad1cc650c42e8e29f4e5446fb15274
naver0504 <qortndwls1234@gmail.com>
2023-09-18 14:36:57 +0900

    하루 사진들 받아오는 함수 작성

--- src/main/java/com/fooddiary/api/repository/ImageRepository.java
+++ src/main/java/com/fooddiary/api/repository/ImageRepository.java
@@ -2,10 +2,18 @@ package com.fooddiary.api.repository;
 
 import com.fooddiary.api.entity.image.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ImageRepository extends JpaRepository<Image, Integer> {
 
+    @Query("select i from Image i join DayImage d " +
+            "where d.time.year = :year and d.time.month = :month and d.time.day = :day and d.user.id = :userId and i.parentImage is null")
+    List<Image> findByYearAndMonthAndDay(@Param("year") int year,
+                                         @Param("month") int month, @Param("day") int day, @Param("userId") int userId);
 
 }
