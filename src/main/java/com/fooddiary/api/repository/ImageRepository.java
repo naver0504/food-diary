5b8d2afa6213749242832c31cbd6f0324234ae30
naver0504 <qortndwls1234@gmail.com>
2023-09-25 19:07:38 +0900

    기존 함수 querydsl로 대체

--- src/main/java/com/fooddiary/api/repository/ImageRepository.java
+++ src/main/java/com/fooddiary/api/repository/ImageRepository.java
@@ -7,13 +7,18 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ImageRepository extends JpaRepository<Image, Integer> {
 
-    @Query("select i from Image i join DayImage d " +
-            "where d.time.year = :year and d.time.month = :month and d.time.day = :day and d.user.id = :userId and i.parentImage is null")
-    List<Image> findByYearAndMonthAndDay(@Param("year") int year,
-                                         @Param("month") int month, @Param("day") int day, @Param("userId") int userId);
+
+    @Query("select i from Image i left join fetch i.tags T where i.id = :id and i.user.id = :userId")
+    Optional<Image> findByIdWithTag(@Param("id") int id, @Param("userId") int userId);
+
+    @Query("select i from Image i where i.parentImage.id = :id order by i.id asc")
+    List<Image> findByParentImageId(@Param("id") int id);
+
+
 
 }
