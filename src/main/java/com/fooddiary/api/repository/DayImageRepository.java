7c126684f1d3595ba781371e6c6b665bfbd87fc5
naver0504 <qortndwls1234@gmail.com>
2023-10-03 23:28:53 +0900

    findByImageIdAndUserId 메소드 작성

--- src/main/java/com/fooddiary/api/repository/DayImageRepository.java
+++ src/main/java/com/fooddiary/api/repository/DayImageRepository.java
@@ -7,10 +7,15 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 
 @Repository
 public interface DayImageRepository extends JpaRepository<DayImage, Integer> {
+
+    @Query("select d from DayImage d join  d.images i where i.id = :imageId and d.user.id = :userId")
+    Optional<DayImage> findByImageIdAndUserId(@Param("imageId") int imageId, @Param("userId") int userId);
+
 
     @Query("select d from DayImage d where d.time.year = :year and d.time.month = :month and d.user.id = :userId order by d.time.day asc")
     List<DayImage> findByYearAndMonth(@Param("year") int year, @Param("month") int month, @Param("userId") int userId);
@@ -19,17 +24,5 @@ public interface DayImageRepository extends JpaRepository<DayImage, Integer> {
             "where d.time.year = :year and d.time.month = :month and d.time.day = :day and d.user.id = :userId ")
     DayImage findByYearAndMonthAndDay(@Param("year") int year,
                                              @Param("month") int month, @Param("day") int day, @Param("userId") int userId);
-
-
-
-    @Query("select count(*) from DayImage d where d.user.id = :userId")
-    int getDayImageCount(@Param("userId") int userId);
-
-
-    @Query("select d from DayImage d join fetch d.images  " +
-            "where d.time.year = :year and d.time.month = :month and d.user.id = :userId " +
-            "order by d.time.day desc ")
-    List<DayImage> findDayImageByYearAndMonthOrderByDayDesc(@Param("year") int year, @Param("month") int month, @Param("userId") int userId);
-
 
 }
