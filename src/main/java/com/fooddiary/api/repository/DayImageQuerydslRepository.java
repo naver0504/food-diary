8e99d51d6d43bf4668717cb5e53758325b8197d6
naver0504 <qortndwls1234@gmail.com>
2023-10-03 23:28:35 +0900

    getTimeLineDayImage 메소드 작성

--- src/main/java/com/fooddiary/api/repository/DayImageQuerydslRepository.java
+++ src/main/java/com/fooddiary/api/repository/DayImageQuerydslRepository.java
@@ -1,8 +1,11 @@
 package com.fooddiary.api.repository;
 
+import com.fooddiary.api.entity.image.DayImage;
 import com.fooddiary.api.entity.image.Time;
+import com.fooddiary.api.entity.user.QUser;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 
 import java.util.HashMap;
@@ -13,12 +16,28 @@ import static com.fooddiary.api.entity.image.QDayImage.*;
 import static com.fooddiary.api.entity.image.QImage.*;
 
 @Repository
+@Slf4j
 public class DayImageQuerydslRepository {
 
     private final JPAQueryFactory jpaQueryFactory;
 
     public DayImageQuerydslRepository(EntityManager entityManager) {
         jpaQueryFactory = new JPAQueryFactory(entityManager);
+    }
+
+    public List<DayImage> getTimeLineDayImage(final int year, final int month, final int startDay, final int userId) {
+        return jpaQueryFactory.selectFrom(dayImage)
+                .where(
+                        dayImage.user.id.eq(userId),
+                        dayImage.time.year.eq(year),
+                        dayImage.time.month.eq(month),
+                        dayImage.time.day.lt(startDay)
+                )
+                .orderBy(dayImage.time.day.desc())
+                .limit(4)
+                .fetch();
+
+
     }
 
     public Time getTime(final int imageId) {
@@ -70,5 +89,13 @@ public class DayImageQuerydslRepository {
 
     }
 
+    public boolean existByUserId(final int userId) {
+        Integer fetchFirst = jpaQueryFactory
+                .selectOne()
+                .from(dayImage)
+                .join(dayImage.user, QUser.user)
+                .fetchFirst();
 
+        return fetchFirst != null;
+    }
 }
