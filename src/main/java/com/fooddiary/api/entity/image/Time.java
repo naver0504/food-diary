57575aad2c9eb949eea16b2eae83e97802fe58f3
naver0504 <qortndwls1234@gmail.com>
2023-10-07 19:00:43 +0900

    Time 관련 메소드 작성

--- src/main/java/com/fooddiary/api/entity/image/Time.java
+++ src/main/java/com/fooddiary/api/entity/image/Time.java
@@ -5,6 +5,7 @@ import lombok.*;
 
 import java.time.LocalDateTime;
 
+
 @Getter
 @Embeddable
 @NoArgsConstructor
@@ -23,4 +24,32 @@ public class Time {
         this.day = dateTime.getDayOfMonth();
         this.localDateTime = dateTime;
     }
+
+    public static LocalDateTime getDateTime(final int year, final int month, final int day) {
+        return LocalDateTime.of(year, month, day, 0, 0);
+    }
+    public static String getDayOfWeek(final Time time) {
+        int dayOfWeek = Time.getDateTime(time.getYear(), time.getMonth(), time.getDay())
+                .getDayOfWeek().getValue();
+
+        switch (dayOfWeek) {
+            case 1:
+                return "월";
+            case 2:
+                return "화";
+            case 3:
+                return "수";
+            case 4:
+                return "목";
+            case 5:
+                return "금";
+            case 6:
+                return "토";
+            case 7:
+                return "일";
+            default:
+                throw new RuntimeException("요일을 찾을 수 없습니다.");
+        }
+    }
+
 }
