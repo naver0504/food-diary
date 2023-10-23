b96260a4b1f24cf07c9729eb1404a070c679ef34
jasuil <jasuil@daum.net>
2023-10-23 22:50:24 +0900

    타임라인 쿼리 변경

--- src/main/java/com/fooddiary/api/repository/timeline/TimelineRepository.java
+++ src/main/java/com/fooddiary/api/repository/timeline/TimelineRepository.java
@@ -2,6 +2,8 @@ package com.fooddiary.api.repository.timeline;
 
 import com.fooddiary.api.dto.response.timeline.TimelineDiaryImageSQLDTO;
 import com.fooddiary.api.entity.diary.Diary;
+
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -13,23 +15,22 @@ import java.util.List;
 @Repository
 public interface TimelineRepository extends JpaRepository<Diary, Integer> {
 
-    @Query(value = "select\n" +
-            "        d1_0.id,\n" +
-            "        d1_0.create_time,\n" +
-            "        i1_0.stored_file_name, \n" +
-            "        i1_0.id as image_id\n" +
-            "    from\n" +
-            "        diary d1_0 \n" +
-            "    join\n" +
-            "        (select * from image order by image.update_at desc limit 1) i1_0 \n" +
-            "    on (i1_0.diary_id = d1_0.id) \n" +
-            "    where\n" +
-            "        d1_0.user_id = :userId \n" +
-            "        and d1_0.create_time between :startDate and :endDate\n" +
-            "    order by\n" +
-            "        d1_0.create_time asc,\n" +
-            "        i1_0.update_at desc ", nativeQuery = true)
+    // https://stackoverflow.com/questions/2129693/using-limit-within-group-by-to-get-n-results-per-group
+    @Query(// + "x.diaryId, " 쿼리 디버깅용
+// + ", x.update_at as imageUpdateAt \n" 쿼리 디버깅용
+            value = """
+                    select diary.id, diary.create_time as createTime,\s
+                    x.stored_file_name as storedFileName, x.id as imageId from (
+                     select diary_id, stored_file_name, id, update_at, row_number() over (partition by diary_id order by update_at desc) as n\s
+                     from image where (
+                     diary_id in (select id from diary where user_id = :userId and create_time between :startDate and :endDate)
+                     )
+                    ) as x
+                    inner join diary
+                    on (diary.id = x.diary_id)
+                    where n <= 1
+                    order by diary.id asc, x.update_at desc\s""", nativeQuery = true)
     List<TimelineDiaryImageSQLDTO> getTimeLineDiaryWithLatestImage(@Param("userId") int userId,
                                                                    @Param("startDate") LocalDateTime startDate,
-                                                                   @Param("endDate") LocalDateTime endDate);
+                                                                   @Param("endDate") LocalDateTime endDate, Pageable page);
 }
