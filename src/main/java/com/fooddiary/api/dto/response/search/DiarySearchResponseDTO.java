eaf1c775e875b33e69082202ba41dd0fe38a351e
naver0504 <qortndwls1234@gmail.com>
2023-11-01 15:40:33 +0900

    DiarySearchResponseDTO 클래스 작성

---
+++ src/main/java/com/fooddiary/api/dto/response/search/DiarySearchResponseDTO.java
@@ -0,0 +1,17 @@
+package com.fooddiary.api.dto.response.search;
+
+import com.fooddiary.api.dto.response.timeline.TimelineDiaryDTO;
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class DiarySearchResponseDTO {
+
+    private String categoryName;
+    private List<TimelineDiaryDTO> diaryList;
+}
