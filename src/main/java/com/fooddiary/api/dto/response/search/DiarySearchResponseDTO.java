b6bacf85579c51f58b468e5ec3b78ca52f6490fc
naver0504 <qortndwls1234@gmail.com>
2023-11-22 14:04:48 +0900

    CategoryType 추가

--- src/main/java/com/fooddiary/api/dto/response/search/DiarySearchResponseDTO.java
+++ src/main/java/com/fooddiary/api/dto/response/search/DiarySearchResponseDTO.java
@@ -1,5 +1,6 @@
 package com.fooddiary.api.dto.response.search;
 
+import com.fooddiary.api.dto.request.search.CategoryType;
 import com.fooddiary.api.dto.response.timeline.TimelineDiaryDTO;
 import lombok.*;
 
@@ -13,6 +14,8 @@ import java.util.List;
 public class DiarySearchResponseDTO {
 
     private String categoryName;
+    private CategoryType categoryType;
+
     private int count;
     private List<TimelineDiaryDTO> diaryList;
 }
