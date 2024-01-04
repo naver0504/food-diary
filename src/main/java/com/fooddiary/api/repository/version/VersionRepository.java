c6587875aea2e5bc4d0d97dcdcc3d79e23fb9b49
naver0504 <qortndwls1234@gmail.com>
2024-01-04 23:40:44 +0900

    쿼리 실행 시 오류로 인해 @Param 추가

--- src/main/java/com/fooddiary/api/repository/version/VersionRepository.java
+++ src/main/java/com/fooddiary/api/repository/version/VersionRepository.java
@@ -3,12 +3,13 @@ package com.fooddiary.api.repository.version;
 import com.fooddiary.api.entity.version.Version;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
 
 public interface VersionRepository extends JpaRepository<Version, Integer> {
 
-    @Query("select v.version from Version v where v.releaseAt <= :now and v.isRelease = true order by v.releaseAt desc limit 1")
-    String findLatestReleaseVersion(LocalDateTime now);
+    @Query("select v.version from Version as v where v.releaseAt <= :now and v.isRelease = true order by v.releaseAt desc limit 1")
+    String findLatestReleaseVersion(@Param("now") LocalDateTime now);
 
 }
